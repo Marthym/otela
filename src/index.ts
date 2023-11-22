ffb37c7c50a462d8af4897d4b2bbb05467e4b1c3
Marthym <marthym@gmail.com>
2023-11-22 21:38:39 +0100

    feat(main): configure otel to transfert span to metrics
    improve attributes

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,18 @@ import { WebTracerProvider } from '@opentelemetry/sdk-trace-web';
 import { ZoneContextManager } from '@opentelemetry/context-zone';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
 import Bowser from 'bowser';
+import { SpanKind, SpanStatusCode } from '@opentelemetry/api';
+import { Resource } from '@opentelemetry/resources';
 
-export function tag(target?: string): void {
+export function tag(service: string, target?: string): void {
     console.debug('Start page tagging...');
-    const provider = new WebTracerProvider();
+    const provider = new WebTracerProvider({
+        resource: new Resource({
+            'service.name': service,
+        }),
+    });
     const exporter = (target) ?
-        new OTLPTraceExporter({
-            url: target,
-        }) :
+        new OTLPTraceExporter({ url: target }) :
         new OTLPTraceExporter();
     provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
     provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
@@ -20,18 +24,22 @@ export function tag(target?: string): void {
     });
 
     const browser = Bowser.parse(window.navigator.userAgent);
-    const span = provider.getTracer(document.location.pathname).startSpan('documentLoad');
-    span.setAttributes({
-        title: document.title,
-        domain: document.location.host,
-        path: document.location.pathname,
-        navigator: browser.browser.name,
-        os: browser.os.name,
-        platform: browser.platform.type,
+    const span = provider.getTracer(document.location.pathname).startSpan('documentLoad', {
+        attributes: {
+            service_name: document.location.host,
+            title: document.title,
+            domain: document.location.host,
+            path: document.location.pathname,
+            navigator: browser.browser.name,
+            os: browser.os.name,
+            platform: browser.platform.type,
+        },
+        kind: SpanKind.CLIENT,
     });
 
     window.addEventListener('load', async () => {
-        console.debug('STARTING ANALYTICS');
+        console.debug('...end tagging');
+        span.setStatus({ code: SpanStatusCode.OK });
         span.end();
     });
 }
